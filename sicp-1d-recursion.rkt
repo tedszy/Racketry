1236d2f0ae1b41073cd34d1903dc6fab96a54df9
Ted Szylowiec <tedszy@gmail.com>
2018-05-16 20:31:14 +0700

    added 1d

---
+++ sicp-1d-recursion.rkt
@@ -0,0 +1,16 @@
+;;; sicp-1d-recursion.rkt
+
+#lang racket
+
+(require "check.rkt"
+         "simple-table.rkt")
+
+
+;; Exercise 1.10 ========================================
+
+
+;; Exercise 1.11 ========================================
+
+
+;; Exercise 1.12 ========================================
+
