92bcdee02525c2939683941cab2503947e129034
Ted Szylowiec <tedszy@gmail.com>
2018-05-26 09:08:28 +0700

    refactored simple-table

--- simple-table.rkt
+++ simple-table.rkt
@@ -39,45 +39,96 @@
 (define (get-max-column-widths table)
   (map get-max-length-of-string-list (transpose table)))
 
-(define standard-column-separator " | ")
-(define standard-row-separator "\n")
+;; Takes a table of strings and returns a table where
+;; all columns have equal string widths, and have been
+;; aligned in a given way. Alignments default to right
+;; if not specified by keyword parameter.
+(define (normalize-columns table #:align [alignments null])
+  (map  (lambda (row) 
+          (map format-string row 
+               (get-max-column-widths table) 
+               (if (null? alignments)
+                   (make-list (length (car table)) 'right)
+                   alignments)))
+        table))
 
+;; Put item in between elements of mylist.
+;; > (intersperse '(1 2 3 4) 'zarf)
+;; '(1 zarf 2 zarf 3 zarf 4)
+;; We use this for putting column separators.
+(define (intersperse mylist item)
+  (let loop ((mylist mylist) (result '()))
+    (if (null? (cdr  mylist))
+        (reverse (cons (car mylist) result))
+        (loop (cdr mylist) 
+              (cons item
+                    (cons (car mylist) 
+                          result))))))
 
-;; If alignment list is not specified, alignments 
-;; default to right for every cell.
-(define (print-table table #:align [alignments null])
-  (let ((column-widths (get-max-column-widths table))
-        (alignments (if (null? alignments)
-                        (make-list (length (car table)) 'right)
-                        alignments))
-        (out (open-output-string)))
-    (for-each (lambda (row)
-                (let loop ((row row)
-                           (column-widths column-widths)
-                           (alignments alignments))
-                  (let ((cell-text (format-string (car row)
-                                                  (car column-widths)
-                                                  (car alignments))))
-                    (cond ((null? (cdr row))
-                           ;; We are at the last cell of the row,
-                           ;; so handle it differently. Put row
-                           ;; separator instead of column separator.
-                           ;; And don't recurse. We're done with the row.
-                           (fprintf out "~a~a"
-                                    cell-text
-                                    standard-row-separator))
-                          (else
-                           (fprintf out "~a~a"
-                                    cell-text
-                                    standard-column-separator)
-                           (loop (cdr row)
-                                 (cdr column-widths)
-                                 (cdr alignments)))))))
-              table)
-    (display (get-output-string out))))
+(define (print-table table #:align [alignments null] #:head [head #f] #:bars [bars #f])
+  (let ((column-separator (if bars " | " "  "))
+        (head-column-separator (if bars "   " "  "))
+        (normalized-table (normalize-columns table #:align alignments)))
+    (for-each displayln 
+             (map (lambda (u) 
+                    (string-join u ""))
+                  (if head
+                      ;; If head=true, then the first row has to be handled
+                      ;; differently from the body of the table. No bars and 
+                      ;; with underline.
+                      (let ((header-row (intersperse (car normalized-table) 
+                                                     head-column-separator)))
+                        (cons header-row 
+                              ;; Add the underline.
+                              (cons (map (lambda (u) 
+                                           (make-string (string-length u) #\-))
+                                         header-row)
+                                    ;; Add the body of the table.
+                                    (map (lambda (row)
+                                           (intersperse row column-separator)) 
+                                         (cdr normalized-table)))))
+                      ;; No special header.
+                      (map (lambda (row)
+                             (intersperse row column-separator))
+                           normalized-table))))))
 
-;; test
 (define tt '(("zarf" "greeblies" "terwilligbert")
              ("1" "2" "3")
              ("10" "20" "30")
              ("100" "200" "300")))
+
+;; Usage.
+;;
+;; > (print-table tt)
+;; zarf  greeblies  terwilligbert
+;;   1          2              3
+;;  10         20             30
+;; 100        200            300
+;;
+;; > (print-table tt #:bars #t)
+;; zarf | greeblies | terwilligbert
+;;    1 |         2 |             3
+;;   10 |        20 |            30
+;;  100 |       200 |           300
+;;
+;; > (print-table tt #:head #t)
+;; zarf  greeblies  terwilligbert
+;; ------------------------------
+;;    1          2              3
+;;   10         20             30
+;;  100        200            300
+;;
+;; > (print-table tt #:head #t #:bars #t)
+;; zarf   greeblies   terwilligbert
+;; --------------------------------
+;;    1 |         2 |             3
+;;   10 |        20 |            30
+;;  100 |       200 |           300
+;;
+;; > (print-table tt #:head #t #:bars #t #:align '(left center right))
+;; zarf   greeblies   terwilligbert
+;; --------------------------------
+;; 1    |     2     |             3
+;; 10   |    20     |            30
+;; 100  |    200    |           300
+
