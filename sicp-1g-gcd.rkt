c14b9cbb8f1a52a1d7362775a2e39e41ee749e40
Ted Szylowiec <tedszy@gmail.com>
2018-05-30 19:29:58 +0700

    notes on Lame theorem

--- sicp-1g-gcd.rkt
+++ sicp-1g-gcd.rkt
@@ -5,6 +5,9 @@
 (require "check.rkt"
          "simple-table.rkt")
 
+;; Euclidean algorithm. m = q*d + r.
+;; m = dividend, q = quotient,
+;; d = divisor, r = remainder.
 (define (gcd1 a b)
   (define (stringify a b)
     (map number->string (list a (quotient a b) b (remainder a b))))
@@ -28,6 +31,25 @@
 ;;   65   2   30    5
 ;;   30   6    5    0
 ;; 5
+
+;; Lame's theorem.
+;; Take n = min(a, b)
+;; Assume the Euclidean algorithm for gcd(a,b) 
+;; takes k steps. Then we have,
+;;
+;;   n >= F(k)
+;;
+;; But since F(k) -> (p^k)/sqrt(5), then 
+;;
+;;   n >= p^k * 1/sqrt(5)
+;; 
+;; so log(n) >= k * some constant.
+;; Therefore gcd steps, k, grows like Theta(log(n)).
+
+
+
+
+
 
 
 ;; Exercise 1.20 ========================================
