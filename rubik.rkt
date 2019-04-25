83379cc58b106fe92958a0dfd78962e2af52cdcf
Ted Szylowiec <tedszy@gmail.com>
2019-04-25 15:54:26 +0700

    cube organization

--- rubik.rkt
+++ rubik.rkt
@@ -8,6 +8,12 @@
 
 (provide (all-defined-out))
 
+;; Cube organization:
+;;
+;; layers:
+;;    front - middle - back
+;;    left - middle - right
+;;    top - middle - bottom
 
 ;; This is what my cube looks like.
 ;;
@@ -78,37 +84,68 @@
 ;; Up, down, left, right, back faces can ALWAYS be identified by looking 
 ;; at the center face! So even if you rotated your cube as you have been 
 ;; playing with it, you can ALWAYS return it to the standard orientation: 
-;; up pointing up, front pointing toward you, back pointing away from you, etc. 
+;; up pointing up, front pointing toward you, back pointing away from you. 
 ;; This is a very important point! You've got to imagine this carefully!
 
 ;; ---------------------------------------------------------------------
 
 ;; The next thing you notice when you examine your Rubik's cube is that 
-;; it is made of many little cubes. We will call these little cubes *minicubes*.
-;; Count them: there are 26 minicubes in all:
+;; it is made of many little cubes. We will call these little cubes
+;; *minicubes*. Count them: there are 26 minicubes in all:
 ;;
 ;;   * 9 on the front layer.
 ;;   * 8 on the middle layer.
 ;;   * 9 on the back layer.
 ;;
-;; Of these, the 6 center cubes are immovable, the other 20 are moveable.
-
 ;; These minicubes are of three types:
 ;;
-;;   * center cubes
-;;   * edge cubes
-;;   * corner cubes.
+;;   * 6 center cubes, 1 facet each.
+;;   * 12 edge cubes, 2 facets each.
+;;   * 8 corner cubes, 3 facets each.
+;;
+;; Of these, the 6 center cubes are immovable, the other 20 are moveable.
+;; We don't consider the center minicubes because, again, they don't move.
+;;
+;; Corner cubes:
+;;
+;;   Up face:
+;;   1,9,35  <=> white, red, blue
+;;   3,27,33 <=> white, orange, blue
+;;   8,19,25 <=> white, orange green
+;;   6,11,17 <=> white, red, green
+;;
+;;   Down face:
+;;   14,40,46 <=> red, blue, yellow
+;;   16,22,41 <=> red, green, yellow
+;;   24,30,43 <=> green, orange, yellow
+;;   32,38,48 <=> orange, blue, yellow
+;;
 
-
-
-
-
-
+;; Edge cubes:
+;;
+;; Top layer:
+;;   2,34 <=> white, blue
+;;   4,10 <=> white, red
+;;   5,26 <=> white, orange
+;;   7,18 <=> white, green
+;;
+;; Middle layer:
+;;   12,37 <=> red, blue
+;;   13,29 <=> red, green
+;;   21,28 <=> green, orange
+;;   29,36 <=> orange, blue
+;;
+;; Bottom layer:
+;;   15,44 <=> red, yellow
+;;   23,42 <=> green, yellow
+;;   31,45 <=> orange, yellow
+;;   39,47 <=> blue, yellow
 
 
 
 
 ;; ----------------------------------------------------------------------
 
-;; Turning a face cycles the facets of that face and also of the 4 adjacent 
-;; faces. That's why there are 5 cycles involved in the definition of a generator.
+;; Turning a face cycles the facets of that face and also facets of the
+;; 4 adjacent faces. Therefore there are 5 cycles involved in the
+;; definition of a generator.
