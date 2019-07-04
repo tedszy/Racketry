6434eb0e886c15fc1dc32b27ecd65a2ff1a722e1
Ted Szylowiec <tedszy@gmail.com>
2019-07-04 19:54:20 +0700

    smapn: map over many streams

--- sicp-35-streams-api.rkt
+++ sicp-35-streams-api.rkt
@@ -7,6 +7,7 @@
          scdr
          sref
          smap
+         smapn
          sfor-each
          smake-interval
          sfilter
@@ -97,7 +98,69 @@
                        (loop (scdr s) (sub1 n)))))))
 
 
-;; Test.
-(define ones (scons 1 ones))
-(define foo (smake-interval 10 15))
+;; Exercise 3.50 ========================================
+
+;; Generalize smap so it maps over many streams (in args).
+
+(define (smapn fn . args)
+  (if (snull? (car args))
+      the-empty-stream
+      (scons
+       (apply fn (map scar args))
+       ;;(apply smapn (cons fn (map scdr args)))
+       (apply smapn fn (map scdr args)))))
+
+;; The last two lines merit discussion. In 
+;; 
+;;   (apply smapn (cons fn (map scdr args)))
+;;
+;; motice how fn is passed to smapn by a clever trick. 
+;; The function fn is consed onto a list of updated 
+;; (cdr'd) streams, and then smapn is applied to this list. 
+;; Thus fn ends up being the first argument to smapn. This 
+;; allowed us to pass the scdr's of an unspecified 
+;; number of streams into the recursive call to smapn,
+;;
+;; So 
+;;
+;;   (apply smapn (cons fn (map scdr args))) 
+;;
+;; is equivalent to
+;;
+;;   (apply smapn (list fn (scdr s1) (scdr s2) (scdr s3) ...))
+;;
+;; and that's the same as
+;;
+;;   (smapn fn (scdr s1) (scdr s2) (scdr s3) ...)
+;;
+;; where s1, s2, s3 are the streams in the arg list. 
+;; But since doing a recursive call to a function with
+;; a variable argument list is a common need, there exists
+;; an extended 'apply' syntax that handles it! 
+;;
+;;   (apply smapn fn (map scdr args)))))
+;;
+;; fn is consed onto the list returned by map. 
+;; You can even do things like:
+;;
+;;   (apply + 1 2 3 '(4 5 6)) ==> 21.
+;; 
+;; Perhaps this ability of apply didn't exist in SICP scheme.
+;;
+;; Test:
+;;
+;; (define foo (smake-interval 1 100))
+;;   ==> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...]
+
+;; (define goo (scdr foo))
+;;   ==> [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, ...]
+
+;; (define soo (scdr goo))
+;;   ==> [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, ...]
+
+;; (ls (smapn + foo goo soo))
+;;  ==> [6, 9, 12, 15, 18, 21, 24, 27, 30, 33, ...]
+
+
+
 
