6b52c34264ebfeaa5a30227f288d2e3d3eb4a00d
Ted Szylowiec <tedszy@gmail.com>
2019-07-06 19:26:55 +0700

    further sketch of ex 3.51

--- sicp-35b.rkt
+++ sicp-35b.rkt
@@ -14,14 +14,40 @@
 ;;   evaluated when it is accessed by the [sicp-]stream-cdr
 ;;   procedure rather than when the stream is constructed
 ;;   by [sicp-]cons-stream."
-
+;;
+;; Look at the way sicp-streams are constructed in the stream
+;; api module: 
+;;
+;;   (define-syntax sicp-stream-cons
+;;     (syntax-rules ()
+;;       ((_ a b) (cons a (delay-plain b)))))
+;;
+;;   (define (sicp-stream-car s) (car s))
+;;   (define (sicp-stream-cdr s) (my-force (cdr s)))
+;;
+;;   (define (sicp-stream-map proc s)
+;;     (if (sicp-stream-null? s)
+;;         sicp-empty-stream
+;;         (sicp-stream-cons (proc (sicp-stream-car s))
+;;                (sicp-stream-map proc (sicp-stream-cdr s)))))
+;;
+;; 
 
 
 
 
 (define (show u) (newline) (display u) u)
-(define x (sicp-stream-map show (sicp-stream-make-interval 0 10)))
+(define foo (sicp-stream-cons (show 1) (show 2)))
 
+
+
+
+
+
+
+;; (define x (sicp-stream-map show (sicp-stream-make-interval 0 10)))
+;; (scip-stream-ref x 5)
+;; (sicp-stream-ref x 7)
 
 
 
