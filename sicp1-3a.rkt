8428f9d35695cfe5314f6e2810c870d72ed6c362
Ted Szylowiec <tedszy@gmail.com>
2020-04-20 03:40:43 +0700

    exercises 1.29 to 1.33

--- sicp1-3a.rkt
+++ sicp1-3a.rkt
@@ -1,6 +1,13 @@
 ;;; sicp1-3a.rkt
 ;;;
-;;; Exercises ... 
+;;; Exercises 1.29 -- 1.33
+;;;
+;;; Sum abstraction, tail-recursive sum abstraction,
+;;; numerical integration, Simpson's method, product
+;;; abstaction, factorial, Wallis product for pi,
+;;; generalized sum/product: accumulate, filtered accumulate.
+;;;
+;;; Interesting point from SICP:
 ;;;
 ;;; We don't want to be limited to expressing 5x5x5 for
 ;;; a particular number, 5. We want to express the concept
@@ -8,13 +15,11 @@
 ;;; concept of summing the cubes of things.
 
 
-
 ;; to do
-
+;;
 ;; Get rid of check.rkt
 ;; get rid of format-test/simple
 ;; put table formatting and function timer in sicp lib.
-;; put multiple-value checker in lib
 
 
 #lang racket
@@ -22,11 +27,13 @@
 (require rackunit
          "format-table.rkt")
 
-
 ;; Some beautiful examples of direct recursion.
 
-;; S(a, b) = a + S(a+1, b)
-;;  S(b,b) = 0.
+;; Summing integers from a to b.
+;; Recursion 'equation of motion' and initial condition.
+;; 
+;;   S(a, b) = a + S(a+1, b)
+;;   S(b,b) = 0.
 
 (define (sum-integers a b)
   (if (> a b)
@@ -75,11 +82,8 @@
 ;; Racket has add1 which is like SICP's inc.
 ;; Also has identity.
 
-(define (sum-cubes-1 a b)
-  (sum cube a add1 b))
-
-(define (sum-integers-1 a b)
-  (sum identity a add1 b))
+(define (sum-cubes-1 a b) (sum cube a add1 b))
+(define (sum-integers-1 a b) (sum identity a add1 b))
 
 ;; Not really supposed to use lambdas here because
 ;; SICP didn't introduce them yet. But still...
@@ -169,11 +173,157 @@
 
 ;; Exercise 1.30 ========================================
 
+;; Tail-recursive version of sum. I use the term
+;; 'state-variable recursion' because it uses arguments
+;; exactly as if they were state. This is, I think, a better
+;; descriptive term than tail-recursion or the 'iterative'
+;; terminology of SICP.
+
+(define (sum1 term start next finish)
+  (define (sum-helper a result)
+    (if (> a finish)
+        result
+        (sum-helper (next a) (+ (term a) result))))
+  (sum-helper start 0))
+
+;; Using named-let:
+(define (sum2 term start next finish)
+  (let loop ((a start) (result 0))
+    (if (> a finish)
+        result
+        (loop (next a) (+ (term a) result)))))
+
+(check-equal? (sum1 cube 1 add1 100) (sum-cubes 1 100))
+(check-equal? (sum2 cube 1 add1 100) (sum-cubes 1 100))
+
 
 ;; Exercise 1.31 ========================================
+
+;; Let's do the same for products, as we did for sum:
+;; a higher-order function that evaluates products.
+;; Rather than terms we have factors.
+
+;; Direct recursion.
+(define (product factor a next finish)
+  (if (> a finish)
+      1
+      (* (factor a)
+         (product factor (add1 a) next finish))))
+
+;; State variable recursion.
+(define (product1 factor start next finish)
+  (let loop ((a start) (result 1))
+    (if (> a finish)
+        result
+        (loop (next a) (* (factor a) result)))))
+
+(define (factorial n) (product identity 1 add1 n))
+(define (factorial1 n) (product identity 1 add1 n))
+
+(check-equal? (factorial 10) 3628800)
+(check-equal? (factorial1 10) 3628800)
+
+;; Wallis pi/4 infinite product:
+;;
+;;    pi/4 = (2/3)*(4/3)*(4/5)*(6/5)*(6/7)*(8/7)*...
+;;
+
+;; '(3 3 5 5 7 7 9 9 11 ...)
+(define (pi/4-factor-denominator k)
+  (- (* 2 (- k (floor (/ (- k 2) 2)))) 1))
+
+;; (2 4 4 6 6 8 8 10 10 ...)
+(define (pi/4-factor-numerator k)
+  (* 2 (- k (floor (/ (- k 1) 2)))))
+
+(define (pi/4-factor k)
+  (* 1.0
+     (/ (pi/4-factor-numerator k)
+        (pi/4-factor-denominator k))))
+
+;; Need a lot of factors before we get close to pi.
+(check-= (* 4 (product pi/4-factor 1 add1 1000))
+         pi
+         0.01)
 
 
 ;; Exercise 1.32 ========================================
 
+;; A higher abstraction comes from the desire to
+;; join sum and product into one wider concept
+;; called 'accumulate'. Terms combined with + are sums
+;; and terms combined with * are products but in accumulate,
+;; the terms are combined with a general 'combiner' function.
+;;
+;; Sum results begin with 0 and product results begin with 1,
+;; so we also need a general beginning result value 'base-value'.
+;;
+;; Write 'accumulate' and use it to implement sum and product.
+;; Let 'F' mean the generalized equivalent of term and factor.
+
+(define (accumulate combiner base-value F a next finish)
+  (if (> a finish)
+      base-value
+      (combiner (F a)
+                (accumulate combiner
+                            base-value
+                            F
+                            (next a)
+                            next
+                            finish))))
+
+(define (sum3 F a next finish) (accumulate + 0 F a next finish))
+(define (product3 F a next finish) (accumulate * 1 F a next finish))
+
+(check-equal? (sum3 identity 1 add1 10) 55)
+(check-equal? (product3 identity 1 add1 10) 3628800)
+
+;; State-variable recursive accumulate.
+;; It's pretty elegant.
+(define (accumulate1 combiner base-value F start next finish)
+  (let loop ((a start) (result base-value))
+    (if (> a finish)
+        result
+        (loop (next a)
+              (combiner (F a) result)))))
+
+(define (sum4 F a next finish) (accumulate1 + 0 F a next finish))
+(define (product4 F a next finish) (accumulate1 * 1 F a next finish))
+
+(check-equal? (sum4 identity 1 add1 10) 55)
+(check-equal? (product4 identity 1 add1 10) 3628800)
+
 
 ;; Exercise 1.33 ========================================
+
+;; Accumuate F(a) only if a passes some predicate test.
+
+(define (filtered-accumulate
+         predicate? combiner base-value F start next finish)
+  (let loop ((a start) (result base-value))
+    (cond ((> a finish)
+           result)
+          ((predicate? a)
+           (loop (next a)
+                 (combiner (F a) result)))
+          (else
+           (loop (next a) result)))))
+
+;; For the rest we need prime? and gcd. We'll use the Racket ones.
+
+(require math/number-theory)
+
+(define (sum-prime-squares a b)
+  (filtered-accumulate prime? + 0 (lambda (x) (* x x)) a add1 b))
+
+(define (coprime-product n)
+  (filtered-accumulate (lambda (x) (= (gcd x n) 1))
+                       *
+                       1
+                       identity
+                       1
+                       add1
+                       n))
+
+(check-equal? (sum-prime-squares 1 10) 87)
+(check-equal? (coprime-product 10) 189)
